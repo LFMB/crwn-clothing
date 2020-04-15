8f8c59c6d60cb1115f6f8df4379d6423cde515ea
LFMB <lfmblinux@LFMBComp.localdomain>
2020-04-15 13:05:52 -0400

    added redux to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 // import logo from './logo.svg';
 
@@ -10,23 +11,21 @@ import UserAuth from './pages/user-auth/user-auth.component';
 import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 import './App.css';
 
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
+  // use this lifecycle method for all
+  // onAuthStateChanged() or onSnapshot() methods from the auth library
   componentDidMount(){
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
      //  this.setState({currentUser: user });      
       // console.log(user);
@@ -35,26 +34,33 @@ class App extends React.Component {
 
         userRef.onSnapshot(snapShot => {
           // console.log(snapShot.data());
+          /*
           this.setState({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
-          });    
+          });
+          */
+          // redux implementation
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          })
         });
       }
-      this.setState({currentUser: userAuth});
+      setCurrentUser(userAuth);
     });
   }
 
   componentWillUnmount(){
-    this.unsubscribeFromAuth();
+    this.unsubscribeFromAuth();  
   }
 
   render(){
     return (
       <div className="app-wrapper">
-        <Header  currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -65,4 +71,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
