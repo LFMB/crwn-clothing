2f079d3649812882d67e9f07f67e7eb824b6fc2a
LFMB <lfmblinux@LFMBComp.localdomain>
2020-04-19 15:50:42 -0400

    connected cart item and cart dropdown components with cartItems reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
 import FormBtn from '../buttons/form-btn.component';
+import CartItem from '../cart-item/cart-item.component';
+
 
 import './cart-dropdown.styles.scss';
 
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
 	<div className="cart-dropdown">
-		<div className="cart-items" />
+		<div className="cart-items">
+		{
+			cartItems.map(
+				cartItem => (<CartItem key={cartItem.id} item={cartItem} />)
+			)
+		}
+		</div>
 		<FormBtn>Go to Checkout</FormBtn>
 	</div>
 )
 
-export default CartDropdown;
+
+// don't understand difference between mapStateToProps and map dispatch to props
+const mapStateToProps = ({cart: {cartItems}}) =>({
+	cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
