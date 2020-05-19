b984e840467e8cd4d1954b74f906c3cdfb92ff68
LFMB <lfmblinux@LFMBComp.localdomain>
2020-05-19 16:57:59 -0400

    created checkout-item component and added it to checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import { 
+	selectCartItems, 
+	selectCartTotal 
+} from '../../redux/cart/cart.selectors';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import './checkout.styles.scss';
 
@@ -28,7 +32,7 @@ const CheckoutPage = ({cartItems, total}) => (
 		</div>
 		{
 			cartItems.map(cartItem => 
-				cartItem.name
+				<CheckoutItem key={cartItem.id} cartItem={cartItem}/> 
 			)
 		}
 		<div className="checkout-total">
