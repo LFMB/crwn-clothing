32448e107ea2627885dd4a7c61dc08d8b4e9ff79
LFMB <lfmblinux@LFMBComp.localdomain>
2020-05-20 17:18:57 -0400

    added redux-persit to manage localStorage via redux - checkout npm docs on redux-persist package for more info

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
+// user reducer is being managed by firebase so no need to whitelist it
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['cart'] 
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
