6812569aac2d4bff92f8bc74df7a62c004108082
LFMB <lfmblinux@LFMBComp.localdomain>
2020-06-05 18:32:20 -0400

    moved shop components to redux-thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import {withRouter} from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
 import CollectionPage from '../collection/collection.component';
@@ -8,9 +9,14 @@ import CollectionPage from '../collection/collection.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+// import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-import { updateCollections } from '../../redux/shop/shop.actions';
+// import { updateCollections } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { 
+	selectIsCollectionFetching,
+	selectIsCollectionsLoaded 
+} from '../../redux/shop/shop.selectors';
 
 /*
 const ShopPage = ({match}) => {
@@ -29,6 +35,7 @@ const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 
 class ShopPage extends React.Component {
+	/*
 	constructor(){
 		super();
 
@@ -36,8 +43,8 @@ class ShopPage extends React.Component {
 			loading: true
 		}
 	}
-
-	unsubscribeFromSnapshot = null;
+	*/
+// 	unsubscribeFromSnapshot = null;
 
 	/*
 	// observable/ observer pattern
@@ -62,49 +69,51 @@ class ShopPage extends React.Component {
 	// firebase project id: crwn-db-4f090
 	// https://crwn-db-4f090.firebaseio.com end url in .json
 	componentDidMount(){
-		const { updateCollections } = this.props;
-		const collectionRef = firestore.collection('collections');
+		// const { updateCollections } = this.props;
 
 		/*
 		fetch('https://firestore.googleapis.com/v1/projects/crwn-db-4f090/databases/(default)/documents/collections')
 		.then(response => response.json())
 		.then(collections => console.log('collections', collections));
-		*/
-
-		collectionRef.get().then(snapshot => {
-			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-			updateCollections(collectionsMap);
-			this.setState({loading: false});
-		});
-		
-		
+		*/		
+		const { fetchCollectionsStartAsync } = this.props;
+		fetchCollectionsStartAsync();
 	}
 
 	render(){
-		const { match } = this.props;
-		const { loading } = this.state;
+		const { 
+			match, 
+			isFetchingCollections,
+			isCollectionsLoaded } = this.props;
+		// const { loading } = this.state;
 
 		return(
 			<div className="shop-page">
 				<Route 
 					exact path={`${match.path}`} 
 					render={(props) => (
-						<CollectionsOverviewWithSpinner isLoading={loading} {...props}/>
+						<CollectionsOverviewWithSpinner isLoading={isFetchingCollections} {...props}/>
 					)}
 				/>
 				<Route 
 					path={`${match.path}/:collectionId`} 
 					render={(props) => (
-					 <CollectionPageWithSpinner isLoading={loading} {...props}/>
+					 <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props}/>
 					)}
 				/>
 			</div>			
 		)
 	}
 }
-	
-const mapDispatchToProps = dispatch => ({
-	updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+
+const mapStateToProps = createStructuredSelector({
+	isFetchingCollections: selectIsCollectionFetching,
+	isCollectionsLoaded: selectIsCollectionsLoaded,
 })
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = dispatch => ({
+	// updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+	fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
